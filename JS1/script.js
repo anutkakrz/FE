c8e515f0ef1acd89775a3bc46165455ee441afaa
Hanna Krzh <anutkakrz@gmail.com>
2024-01-31 13:44:49 +0200

    31.01

--- JS1/script.js
+++ JS1/script.js
@@ -1,1 +1,61 @@
-alert ("Hello")
+//alert ("Hello")
+// let username = "Anna"
+// console.log(username)
+// console.log("username")
+// const username = prompt("как вас зовут?")
+// console.log(username)
+// const num = 10
+/*console.log(num)
+const username = ("Anna")
+console.log(username)*/
+
+/*const num1 = 100
+const num2 = 50
+const sum = num1+num2
+console.log(sum)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+console.log(num1**2)*/
+
+/*const string1 = "100"
+const string2 = "50"
+console.log(string1+string2)*/
+
+// const firstName = "Anna"
+// const surname = "Krzh"
+// console.log(`Меня зовут ${firstName} ${surname}`)
+
+/*const r = 50
+const g = 70
+const b = 80
+console.log(`rgb(${r}, ${g}, ${b})`)*/
+
+// const username = prompt("Как вас зовут?")
+// console.log(`Пользователя зовут ${username}`)
+
+// const year = prompt("Ваш год рождения?")
+// const vozr = 2023 - year
+// console.log(`Ваш возраст  ${vozr}`)
+
+/*const year = prompt("Ваш год рождения?")
+console.log(`Ваш возраст ${2024-year}`)*/
+
+/*const num = prompt(`Введите число`)
+console.log(`квадрат числа = ${num**2}`)*/
+
+/*const num = +prompt("Введите число")
+const num1 = +prompt("Введите еще одно число")
+const num3 = num + num1
+console.log(`Сумма чисел ${num3}`)*/
+
+/*const degree = 20
+if (degree>15) {
+   console.log("Надень футболку")
+// } */
+
+const num = +prompt("введите число")
+if(num>100) {
+   console.log("Больше")
+}

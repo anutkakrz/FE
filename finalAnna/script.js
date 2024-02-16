e143b6351f11f21d40bf8596719eb874f6ae377c
Hanna Krzh <anutkakrz@gmail.com>
2024-02-16 11:15:39 +0100

    dropmenu css

---
+++ finalAnna/script.js
@@ -0,0 +1,27 @@
+let users = [
+   {
+       firstname: 'John',
+       lastname: 'Smith',
+       age: 19   
+   },
+   {
+       firstname: 'Ben',
+       lastname: 'Smith',
+       age: 16   
+   },
+   {
+       firstname: 'Bob',
+       lastname: 'Brown',
+       age: 20   
+   },
+   {
+       firstname: 'Tom',
+       lastname: 'Hardy',
+       age: 46   
+   },
+   {
+       firstname: 'Lily',
+       lastname: 'Winston',
+       age: 12   
+   }
+]

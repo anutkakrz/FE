7e210d3c3c7d975fd89bb1b60497dac5d5f8754d
Hanna Krzh <anutkakrz@gmail.com>
2024-02-16 16:23:30 +0100

    1

--- finalAnna/script.js
+++ finalAnna/script.js
@@ -1,27 +1,3 @@
-let users = [
-   {
-       firstname: 'John',
-       lastname: 'Smith',
-       age: 19   
-   },
-   {
-       firstname: 'Ben',
-       lastname: 'Smith',
-       age: 16   
-   },
-   {
-       firstname: 'Bob',
-       lastname: 'Brown',
-       age: 20   
-   },
-   {
-       firstname: 'Tom',
-       lastname: 'Hardy',
-       age: 46   
-   },
-   {
-       firstname: 'Lily',
-       lastname: 'Winston',
-       age: 12   
-   }
-]
+let par = document.querySelectorAll("p");
+for (let i = 0; i<par.length; i++ )
+{par[i].innerText = "hello"}
